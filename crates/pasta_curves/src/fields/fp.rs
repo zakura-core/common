a75981a9736cd16820550ba147aaf23fdcd2dfda
Dev Ojha <ValarDragon@users.noreply.github.com>
2026-09-06 00:44:51 +0200

    Require little endian for AArch64 assembly (#383)
    * Require little endian for AArch64 assembly
    
    * Add changelog for AArch64 endian gate

--- crates/pasta_curves/src/fields/fp.rs
+++ crates/pasta_curves/src/fields/fp.rs
@@ -402,7 +402,8 @@ impl Fp {
             feature = "aarch64-asm",
             target_arch = "aarch64",
             target_family = "unix",
-            target_pointer_width = "64"
+            target_pointer_width = "64",
+            target_endian = "little"
         ))]
         {
             Fp(super::aarch64_asm::mul(&self.0, &rhs.0, &MODULUS.0, INV))
@@ -412,7 +413,8 @@ impl Fp {
             feature = "aarch64-asm",
             target_arch = "aarch64",
             target_family = "unix",
-            target_pointer_width = "64"
+            target_pointer_width = "64",
+            target_endian = "little"
         )))]
         {
             self.mul(rhs)
@@ -425,7 +427,8 @@ impl Fp {
             feature = "aarch64-asm",
             target_arch = "aarch64",
             target_family = "unix",
-            target_pointer_width = "64"
+            target_pointer_width = "64",
+            target_endian = "little"
         ))]
         {
             Fp(super::aarch64_asm::square(&self.0, &MODULUS.0, INV))
@@ -435,7 +438,8 @@ impl Fp {
             feature = "aarch64-asm",
             target_arch = "aarch64",
             target_family = "unix",
-            target_pointer_width = "64"
+            target_pointer_width = "64",
+            target_endian = "little"
         )))]
         {
             self.square()
@@ -453,7 +457,8 @@ impl Fp {
             feature = "aarch64-asm",
             target_arch = "aarch64",
             target_family = "unix",
-            target_pointer_width = "64"
+            target_pointer_width = "64",
+            target_endian = "little"
         ))]
         {
             Fp(super::aarch64_asm::sqr_n_mul(
@@ -465,7 +470,8 @@ impl Fp {
             feature = "aarch64-asm",
             target_arch = "aarch64",
             target_family = "unix",
-            target_pointer_width = "64"
+            target_pointer_width = "64",
+            target_endian = "little"
         )))]
         {
             // Leave the accumulator unreduced between squarings. The closing
@@ -485,7 +491,8 @@ impl Fp {
             feature = "aarch64-asm",
             target_arch = "aarch64",
             target_family = "unix",
-            target_pointer_width = "64"
+            target_pointer_width = "64",
+            target_endian = "little"
         ))]
         {
             // Calling the dedicated single-square routine is faster than
@@ -503,7 +510,8 @@ impl Fp {
             feature = "aarch64-asm",
             target_arch = "aarch64",
             target_family = "unix",
-            target_pointer_width = "64"
+            target_pointer_width = "64",
+            target_endian = "little"
         )))]
         {
             Fp(portable::canonicalize(
@@ -839,7 +847,8 @@ impl ff::PrimeField for Fp {
             feature = "aarch64-asm",
             target_arch = "aarch64",
             target_family = "unix",
-            target_pointer_width = "64"
+            target_pointer_width = "64",
+            target_endian = "little"
         ))]
         let tmp = Fp(super::aarch64_asm::from_mont(&self.0, &MODULUS.0, INV));
 
@@ -847,7 +856,8 @@ impl ff::PrimeField for Fp {
             feature = "aarch64-asm",
             target_arch = "aarch64",
             target_family = "unix",
-            target_pointer_width = "64"
+            target_pointer_width = "64",
+            target_endian = "little"
         )))]
         let tmp = Fp::montgomery_reduce(self.0[0], self.0[1], self.0[2], self.0[3], 0, 0, 0, 0);
 
@@ -1020,7 +1030,8 @@ impl ec_gpu::GpuField for Fp {
     feature = "aarch64-asm",
     target_arch = "aarch64",
     target_family = "unix",
-    target_pointer_width = "64"
+    target_pointer_width = "64",
+    target_endian = "little"
 ))]
 fn aarch64_asm_portable_repr(value: Fp) -> [u8; 32] {
     let value = Fp::montgomery_reduce(value.0[0], value.0[1], value.0[2], value.0[3], 0, 0, 0, 0);
@@ -1036,7 +1047,8 @@ fn aarch64_asm_portable_repr(value: Fp) -> [u8; 32] {
     feature = "aarch64-asm",
     target_arch = "aarch64",
     target_family = "unix",
-    target_pointer_width = "64"
+    target_pointer_width = "64",
+    target_endian = "little"
 ))]
 fn aarch64_asm_check_repr(value: Fp) {
     let portable = aarch64_asm_portable_repr(value);
@@ -1050,7 +1062,8 @@ fn aarch64_asm_check_repr(value: Fp) {
     feature = "aarch64-asm",
     target_arch = "aarch64",
     target_family = "unix",
-    target_pointer_width = "64"
+    target_pointer_width = "64",
+    target_endian = "little"
 ))]
 fn aarch64_asm_portable_cmp(lhs: Fp, rhs: Fp) -> core::cmp::Ordering {
     aarch64_asm_portable_repr(lhs)
@@ -1069,7 +1082,8 @@ fn aarch64_asm_portable_cmp(lhs: Fp, rhs: Fp) -> core::cmp::Ordering {
     feature = "aarch64-asm",
     target_arch = "aarch64",
     target_family = "unix",
-    target_pointer_width = "64"
+    target_pointer_width = "64",
+    target_endian = "little"
 ))]
 #[test]
 fn aarch64_asm_matches_portable_arithmetic() {
@@ -1543,7 +1557,8 @@ fn test_from_u512() {
     feature = "aarch64-asm",
     target_arch = "aarch64",
     target_family = "unix",
-    target_pointer_width = "64"
+    target_pointer_width = "64",
+    target_endian = "little"
 ))]
 #[test]
 fn aarch64_asm_mul_unreduced_lhs_matches_portable() {
@@ -1677,7 +1692,8 @@ fn constants_are_canonical() {
     feature = "aarch64-asm",
     target_arch = "aarch64",
     target_family = "unix",
-    target_pointer_width = "64"
+    target_pointer_width = "64",
+    target_endian = "little"
 ))]
 #[test]
 fn aarch64_asm_mul_canonical_sweep_matches_portable() {
@@ -1707,7 +1723,8 @@ fn aarch64_asm_mul_canonical_sweep_matches_portable() {
     feature = "aarch64-asm",
     target_arch = "aarch64",
     target_family = "unix",
-    target_pointer_width = "64"
+    target_pointer_width = "64",
+    target_endian = "little"
 ))]
 #[test]
 fn aarch64_asm_mul_unreduced_lhs_near_modulus_rhs_matches_portable() {
@@ -1755,7 +1772,8 @@ fn aarch64_asm_mul_unreduced_lhs_near_modulus_rhs_matches_portable() {
     feature = "aarch64-asm",
     target_arch = "aarch64",
     target_family = "unix",
-    target_pointer_width = "64"
+    target_pointer_width = "64",
+    target_endian = "little"
 ))]
 #[test]
 #[should_panic(expected = "requires a canonical rhs")]
