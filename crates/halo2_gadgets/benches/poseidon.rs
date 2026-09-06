210ec8d969acc888f55797405e24fa12ceaaa1e9
Dev Ojha <dojha@berkeley.edu>
2026-09-06 12:12:14 +0200

    Update Poseidon benchmark circuit bound

--- crates/halo2_gadgets/benches/poseidon.rs
+++ crates/halo2_gadgets/benches/poseidon.rs
@@ -142,7 +142,7 @@ fn bench_poseidon<S, const WIDTH: usize, const RATE: usize, const L: usize>(
     name: &str,
     c: &mut Criterion,
 ) where
-    S: Spec<Fp, WIDTH, RATE> + Copy + Clone + Sync,
+    S: Spec<Fp, WIDTH, RATE> + Copy + Clone + Sync + 'static,
 {
     // Initialize the polynomial commitment parameters
     let params: Params<vesta::Affine> = Params::new(K);
