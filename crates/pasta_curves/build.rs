9cc64d8f51fd884ffdc915bfc9e62e2785504e5c
Dev Ojha <dojha@berkeley.edu>
2026-09-07 22:53:40 +0200

    Keep required target configuration strict

--- crates/pasta_curves/build.rs
+++ crates/pasta_curves/build.rs
@@ -10,16 +10,16 @@ fn main() {
 
 #[cfg(feature = "aarch64-asm")]
 fn build_aarch64_asm() {
-    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").ok();
-    let target_endian = env::var("CARGO_CFG_TARGET_ENDIAN").ok();
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let target_endian = env::var("CARGO_CFG_TARGET_ENDIAN").unwrap();
     let target_family = env::var("CARGO_CFG_TARGET_FAMILY").ok();
-    let target_os = env::var("CARGO_CFG_TARGET_OS").ok();
-    let target_pointer_width = env::var("CARGO_CFG_TARGET_POINTER_WIDTH").ok();
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let target_pointer_width = env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap();
 
-    if target_arch.as_deref() == Some("aarch64")
-        && target_endian.as_deref() == Some("little")
-        && (target_family.as_deref() == Some("unix") || target_os.as_deref() == Some("none"))
-        && target_pointer_width.as_deref() == Some("64")
+    if target_arch == "aarch64"
+        && target_endian == "little"
+        && (target_family.as_deref() == Some("unix") || target_os == "none")
+        && target_pointer_width == "64"
     {
         cc::Build::new()
             .file("src/asm/pasta_mul-armv8.S")
